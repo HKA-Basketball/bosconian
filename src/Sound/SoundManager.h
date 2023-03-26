f3199c59e99a205ea04d75e8353d4be1357920ea
Deco <82336539+deco42@users.noreply.github.com>
2023-03-26 16:55:32 +0200

    Added SoundManager
    Co-authored-by: MGZeroes <MGZeroes@users.noreply.github.com>

---
+++ src/Sound/SoundManager.h
@@ -0,0 +1,38 @@
+#ifndef BOSCONIAN_SOUNDMANAGER_H
+#define BOSCONIAN_SOUNDMANAGER_H
+
+#include "../../includes.h"
+
+namespace Sound {
+
+    enum : uint32_t
+    {
+        SOUND_BG = 0,
+        //Space for more
+        SOUND_MAX
+    };
+
+    class SoundManager {
+    private:
+        bool initSDL_OA();
+        bool addSound(const void *mem, int size, int soundIndex);
+        bool addSound(const char *file, int soundIndex);
+
+    public:
+        std::vector<Mix_Chunk*> m_sound;
+
+        SoundManager();
+        void playSound(int soundIndex, int volume, int loops = -1);
+        void setVolume(int soundIndex, int volume);
+        void stopSound(int soundIndex);
+        void stopAllSound();
+        void resumeSound(int soundIndex);
+        void freeSound(int soundIndex);
+        void freeAllSounds();
+
+
+    };
+
+} // Sound
+
+#endif //BOSCONIAN_SOUNDMANAGER_H
