03b89c91d16a165c8ff7308ad2666a50fd1dab78
Deco <82336539+deco42@users.noreply.github.com>
2023-05-23 23:29:04 +0200

    Added Player Class for our LocalPlayers
    Co-authored-by: MGZeroes <MGZeroes@users.noreply.github.com>

--- src/Game/Player.h
+++ src/Game/Player.h
@@ -7,8 +7,39 @@ namespace Game {
 
     // Player is a SubClass of Entity
     class Player : public Entity {
+    private:
+        int lives;
+        int score;
+        int currentLevel;
+
     public:
-        // TODO:
+        Player(Utils::Vector2D pos, float deg, Drawing::Texture* img, int lives = 3)
+            : Entity(pos, deg, img, 0), lives(3), score(0), currentLevel(1)
+        {}
+
+        void setLives(int numLives) {
+            lives = numLives;
+        }
+
+        int getLives() const {
+            return lives;
+        }
+
+        void setScore(int playerScore) {
+            score = playerScore;
+        }
+
+        int getScore() const {
+            return score;
+        }
+
+        void setCurrentLevel(int level) {
+            currentLevel = level;
+        }
+
+        int getCurrentLevel() const {
+            return currentLevel;
+        }
     };
 
 } // Game
