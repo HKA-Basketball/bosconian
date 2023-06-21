56b411f200b4643138f11580723c1567cf814bde
Deco <82336539+deco42@users.noreply.github.com>
2023-06-21 09:50:04 +0200

    Relocation of some methods
    Co-authored-by: MGZeroes <MGZeroes@users.noreply.github.com>

--- src/Game/LevelManager.cpp
+++ src/Game/LevelManager.cpp
@@ -1,4 +1,58 @@
 #include "LevelManager.h"
 
 namespace Game {
+
+    void LevelManager::selectLevel(int round) {
+        currentRound = round;
+
+        if (round < 12) {
+            currentLevel = round;
+        } else {
+            int sequenceIndex = (round - 12) % 6;
+            int sequenceLevels[] = {3, 4, 12, 13, 5, 14};
+            currentLevel = sequenceLevels[sequenceIndex];
+        }
+
+        LOG("Round " + std::to_string(round) + " - Selected Level: " + std::to_string(currentLevel));
+    }
+
+    int LevelManager::getCurrentLevel() const {
+        return currentLevel;
+    }
+
+    void LevelManager::updateLevels(std::vector<Utils::Level>& levels) {
+        lvlsInfos = levels;
+    }
+
+    void LevelManager::increaseLevel() {
+        if (currentLevel < 14) {
+            currentLevel++;
+        }
+        else {
+            currentLevel = 1;
+        }
+        LOG("Selected Level: " + std::to_string(currentLevel));
+    }
+
+    void LevelManager::decreaseLevel() {
+        if (currentLevel > 1) {
+            currentLevel--;
+        }
+        else {
+            currentLevel = 14;
+        }
+        LOG("Selected Level: " + std::to_string(currentLevel));
+    }
+
+    void LevelManager::increaseRound() {
+        selectLevel(currentRound+1);
+    }
+
+    Utils::Vector2D LevelManager::getPlayerSpawnLocation() {
+        return lvlsInfos[currentLevel - 1].playerPos;
+    }
+
+    std::vector<Utils::Vector2D> LevelManager::getBaseShipsSpawnLocations() {
+        return lvlsInfos[currentLevel - 1].baseShipsPos;
+    }
 } // Game
--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -1,4 +1,49 @@
 #include "Player.h"
 
 namespace Game {
+
+    void Player::setLives(int numLives) {
+        lives = numLives;
+    }
+
+    int Player::getLives() const {
+        return lives;
+    }
+
+    void Player::setScore(int playerScore) {
+        score = playerScore;
+    }
+
+    int Player::getScore() const {
+        return score;
+    }
+
+    void Player::setCurrentLevel(int level) {
+        currentLevel = level;
+    }
+
+    int Player::getCurrentLevel() const {
+        return currentLevel;
+    }
+
+    void Player::updateProjectiels(float deltaTime) {
+        static Uint64 timeSinceLastProjectile = 0;
+        const Uint64 projectileInterval = 250;
+
+        if (Event::g_event->isKeyClicked(SDL_SCANCODE_LCTRL, false))
+        {
+            Uint64 currentTime = SDL_GetTicks64();
+            if (currentTime - timeSinceLastProjectile >= projectileInterval) {
+                Sound::g_sound->playSound(Sound::SOUND_SHOOT, 3, 0);
+                // Add a new Projectile object to the vector
+                Projectile* newProjectile1 = new Projectile(Utils::GlobalVars::cameraPos.x, Utils::GlobalVars::cameraPos.y
+                        , 1000, Utils::GlobalVars::playerAngle);
+                Projectile* newProjectile2 = new Projectile(Utils::GlobalVars::cameraPos.x, Utils::GlobalVars::cameraPos.y
+                        , 1000, Utils::GlobalVars::playerAngle + 180);
+                addProjectile(newProjectile1);
+                addProjectile(newProjectile2);
+                timeSinceLastProjectile = currentTime;
+            }
+        }
+    }
 } // Game
