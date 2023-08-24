c2b594661ff62a0e626279fa909039ea87583332
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-08-24 23:54:18 +0200

    optimize collision detection
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

--- src/Utilities/GlobalVars.cpp
+++ src/Utilities/GlobalVars.cpp
@@ -97,13 +97,19 @@ namespace Utils {
             return (random * range) + min;
         }
 
-        bool rectIntersect(SDL_Rect rect1, SDL_Rect rect2)
-        {
-            // Check if the bounding boxes of the two intersect
-            return (rect1.x < rect2.x + rect2.w &&
-                    rect1.x + rect1.w > rect2.x &&
-                    rect1.y < rect2.y + rect2.h &&
-                    rect1.y + rect1.h > rect2.y);
+        // Check if the bounding boxes of the two intersect
+        bool rectIntersect(SDL_Rect rect1, SDL_Rect rect2) {
+            if (rect1.x + rect1.w < rect2.x ||
+                rect2.x + rect2.w < rect1.x) {
+                return false; // No horizontal overlap
+            }
+
+            if (rect1.y + rect1.h < rect2.y ||
+                rect2.y + rect2.h < rect1.y) {
+                return false; // No vertical overlap
+            }
+
+            return true; // Intersection
         }
 
         void wrapPos(Vector2D* newPosition) {
