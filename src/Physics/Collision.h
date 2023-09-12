85976688de86e686ce888b57862764093315abb7
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-09-12 17:42:12 +0200

    add comments

--- src/Physics/Collision.h
+++ src/Physics/Collision.h
@@ -8,20 +8,43 @@
 
 namespace Physics {
 
+    /**
+     * @class CollisionManager
+     * @brief Manages collision detection functionalities for the game.
+     *
+     * The CollisionManager class is responsible for handling collision detection
+     * functionalities, which include checking the intersection of two hitboxes and
+     * determining if a projectile hits an entity hitbox.
+     */
     class CollisionManager {
     public:
 
         /**
-         * Check if the two hit boxes intersect
-         * \param hitbox1 The first hitbox.
-         * \param hitbox2 The second hitbox.
-         * \return True if the hitboxes intersect, false otherwise.
+         * @brief Check if two hitboxes intersect.
+         *
+         * This method checks whether two hitboxes intersect using either axis-aligned
+         * bounding box (AABB) collision detection or oriented bounding box (OBB)
+         * collision detection based on the collision mode set in Utils::GlobalVars.
+         *
+         * @param hitbox1 The first hitbox to check.
+         * @param hitbox2 The second hitbox to check.
+         * @return True if the hitboxes intersect, false otherwise.
          */
         static bool checkIntersect(Hitbox hitbox1, Hitbox hitbox2);
+
+        /**
+         * @brief Check if a projectile hits an entity hitbox.
+         *
+         * This method determines whether a projectile, which is active, collides
+         * with an entity hitbox.
+         *
+         * @param projectile A pointer to the projectile to check.
+         * @param entityHitbox The entity hitbox to check against.
+         * @return True if the active projectile hits the entity hitbox, false otherwise.
+         */
         static bool entityHit(Game::Projectile *projectile, Hitbox entityHitbox);
 
     private:
-
         // Helper function to project a polygon onto an axis.
         static void projectPolygon(const Hitbox& hitbox, const Utils::Vector2D& axis, float& min, float& max);
 
@@ -29,6 +52,6 @@ namespace Physics {
         static bool intervalsOverlap(float minA, float maxA, float minB, float maxB);
     };
 
-} // Physics
+} // namespace Physics
 
 #endif //BOSCONIAN_COLLISION_H
