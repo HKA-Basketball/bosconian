2221ac39ff7a02aa782ea2de9c09e7cf5f44610b
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-09-16 19:03:29 +0200

    refactor BaseEntity

--- src/Game/Entitiy/BaseEntity.cpp
+++ src/Game/Entitiy/BaseEntity.cpp
@@ -5,31 +5,66 @@ Game::BaseEntity::BaseEntity(Utils::Vector2D pos, float deg) {
     m_spy = nullptr;
     baseEntities.resize(7);
 
-    std::vector<Utils::Vector2D> hitboxSizeList{{32, 32}, {64, 64}, {64, 64}, {64, 64}, {64, 64}, {64, 64}, {64, 64}};
-
-    std::vector<std::string> baseExpoIMG{"kern", "canon_L", "canon_L_Up", "canon_L_Down", "canon_R", "canon_R_Up", "canon_R_Down"};
-    std::vector<std::string> baseIMG{"kern", "canon_L_norm", "canon_L_Up_norm", "canon_L_Down_norm", "canon_R_norm", "canon_R_Up_norm", "canon_R_Down_norm"};
-    // 0°
-    std::vector<Utils::Vector2D> hitboxPosList{{0, 0}, {0, 0}, {14, -16}, {14, 16}, {0, 0}, {-14, -16}, {-14, 16}};
-    std::vector<Utils::Vector2D> posOffsetList{{0, 0}, {-112, 0}, {-62, -80}, {-62, 80}, {112, 0}, {62, -80}, {62, 80}};
+    std::vector<Utils::Vector2D> hitboxSizeList{{32, 32},
+                                                {64, 64},
+                                                {64, 64},
+                                                {64, 64},
+                                                {64, 64},
+                                                {64, 64},
+                                                {64, 64}};
+    std::vector<std::string> baseExpoIMG{"kern", "canon_L", "canon_L_Up", "canon_L_Down", "canon_R", "canon_R_Up",
+                                         "canon_R_Down"};
+    std::vector<std::string> baseIMG{"kern", "canon_L_norm", "canon_L_Up_norm", "canon_L_Down_norm", "canon_R_norm",
+                                     "canon_R_Up_norm", "canon_R_Down_norm"};
+    std::vector<Utils::Vector2D> hitboxPosList{{0,   0},
+                                               {0,   0},
+                                               {14,  -16},
+                                               {14,  16},
+                                               {0,   0},
+                                               {-14, -16},
+                                               {-14, 16}};
+    std::vector<Utils::Vector2D> posOffsetList{{0,    0},
+                                               {-112, 0},
+                                               {-62,  -80},
+                                               {-62,  80},
+                                               {112,  0},
+                                               {62,   -80},
+                                               {62,   80}};
     std::vector<float> viewOffset{0.f, 180.f, -90.f, 90.f, 0.f, -90.f, 90.f};
 
     if (deg > 1.f) {
-        baseExpoIMG = {"kern_90", "canon_L_90", "canon_L_Up_90", "canon_L_Down_90", "canon_R_90", "canon_R_Up_90", "canon_R_Down_90"};
-        baseIMG = {"kern_90", "canon_L_norm_90", "canon_L_Up_norm_90", "canon_L_Down_norm_90", "canon_R_norm_90", "canon_R_Up_norm_90", "canon_R_Down_norm_90"};
-        // 90°
-        hitboxPosList = {{0, 0}, {0, 0}, {14, 16}, {-14, 16}, {0, 0}, {14, -16}, {-14, -16}};
-        posOffsetList = {{0, 0}, {0, -112}, {80, -62}, {-80, -62}, {0, 112}, {80, 62}, {-80, 62}};
+        for (size_t i = 0; i < baseExpoIMG.size(); ++i) {
+            baseExpoIMG[i] += "_90";
+            baseIMG[i] += "_90";
+        }
+
+        hitboxPosList = {{0,   0},
+                         {0,   0},
+                         {14,  16},
+                         {-14, 16},
+                         {0,   0},
+                         {14,  -16},
+                         {-14, -16}};
+        posOffsetList = {{0,   0},
+                         {0,   -112},
+                         {80,  -62},
+                         {-80, -62},
+                         {0,   112},
+                         {80,  62},
+                         {-80, 62}};
         viewOffset = {0.f, -90.f, 0.f, 180.f, 90.f, 0.f, 180.f};
     }
 
-    for (int i = 0; i < baseIMG.size(); i++) {
-        std::shared_ptr<Drawing::Texture> img = std::make_shared<Drawing::Texture>(baseIMG[i], 0.f, true, "spritesheet.png");
+    for (size_t i = 0; i < baseIMG.size(); ++i) {
+        auto img = std::make_shared<Drawing::Texture>(baseIMG[i], 0.f, true, "spritesheet.png");
 
-        baseEntities[i] = new Entity(pos + posOffsetList[i], 0.f, img, hitboxPosList[i], hitboxSizeList[i], EntityType::Base, (baseIMG[i].find("kern") != std::string::npos) ? 1500 : 200);
+        baseEntities[i] = new Entity(pos + posOffsetList[i], 0.f, img, hitboxPosList[i], hitboxSizeList[i],
+                                     EntityType::Base, baseIMG[i].find("kern") != std::string::npos ? 1500 : 200);
+
         if (baseIMG[i].compare("kern") != std::string::npos) {
             if (Utils::PlayOptions::maxSpy > 0) {
-                std::shared_ptr<Drawing::Texture> img = std::make_shared<Drawing::Texture>("spy", 0.f, true, "spritesheet.png");
+                std::shared_ptr<Drawing::Texture> img = std::make_shared<Drawing::Texture>("spy", 0.f, true,
+                                                                                           "spritesheet.png");
                 m_spy = new Entity((pos + posOffsetList[i]), 0.f, img, EntityType::Moving, 600);
                 m_spy->setBehavior(new SpyBehavior());
                 spy = true;
@@ -37,8 +72,7 @@ Game::BaseEntity::BaseEntity(Utils::Vector2D pos, float deg) {
             }
 
             baseEntities[i]->setBehavior(new CoreBehavior());
-        }
-        else
+        } else
             baseEntities[i]->setBehavior(new CanonBehavior(baseExpoIMG[i], viewOffset[i]));
     }
 }
