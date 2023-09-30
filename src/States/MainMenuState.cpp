404eb07e716bae0b1c73e4247e537628854fb09d
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-09-30 10:29:00 +0200

    change to use Menus enum
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

--- src/States/MainMenuState.cpp
+++ src/States/MainMenuState.cpp
@@ -14,9 +14,9 @@ void MainMenuState::handleInput(float deltaTime) {
 void MainMenuState::update(float deltaTime) {
     MainMenu::Instance()->update();
 
-    MainMenu::Option clickedOption =  MainMenu::Instance()->getClickedOption();
+    Menu::Option clickedOption =  MainMenu::Instance()->getClickedOption();
 
-    if(clickedOption == MainMenu::Option::START) {
+    if(clickedOption == Menu::Option::START) {
         MainMenu::Instance()->reset();
         StateMachine::Instance()->changeState(new PlayingState());
     }
