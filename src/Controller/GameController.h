dc7fa99b286e1e21c76515877ef42753edb3c1be
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-10-03 20:39:23 +0200

    add missing game controller
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

---
+++ src/Controller/GameController.h
@@ -0,0 +1,72 @@
+#ifndef BOSCONIAN_GAMECONTROLLER_H
+#define BOSCONIAN_GAMECONTROLLER_H
+
+#include "../Model/GameModel.h"
+#include "../View/RenderEngine.h"
+#include "../Controller/InputHandler.h"
+#include "../States/StateMachine.h"
+#include "../Utilities/FrameTimer.h"
+#include "../Sound/SoundEngine.h"
+
+class GameController {
+    RenderEngine* renderEngine;
+    SoundEngine* soundEngine;
+
+    InputHandler* inputHandler;
+
+    StateMachine* stateMachine;
+
+    FrameTimer* frameTimer;
+
+public:
+    GameController() {
+        renderEngine = new RenderEngine();
+
+        soundEngine = new SoundEngine();
+        soundEngine->startBackgroundMusic("sounds/loop.wav");
+
+        inputHandler = new InputHandler();
+
+        stateMachine = StateMachine::InitInstance(renderEngine, soundEngine, inputHandler);
+
+        frameTimer = new FrameTimer(60);
+    }
+
+    ~GameController() {
+        soundEngine->stopBackgroundMusic();
+        StateMachine::DestroyInstance();
+
+        delete renderEngine;
+        delete soundEngine;
+        delete inputHandler;
+        delete frameTimer;
+    }
+
+    [[noreturn]] void start() {
+        while(true) loop();
+    }
+
+private:
+    void loop() {
+
+        frameTimer->startFrame();
+
+        inputHandler->update();
+
+        stateMachine->getCurrentState()->handleInput(frameTimer->getDeltaTime());
+
+        stateMachine->getCurrentState()->update(frameTimer->getDeltaTime());
+
+        renderEngine->beginScene();
+
+        stateMachine->getCurrentState()->render();
+
+        renderEngine->endScene();
+
+        frameTimer->delayFrame();
+    }
+
+};
+
+
+#endif //BOSCONIAN_GAMECONTROLLER_H
