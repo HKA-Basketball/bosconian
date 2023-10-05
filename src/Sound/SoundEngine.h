99cc3a684ff21a336940e5cce952fb1d362783b0
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-10-05 08:37:06 +0200

    add comments for Sound Engine
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

--- src/Sound/SoundEngine.h
+++ src/Sound/SoundEngine.h
@@ -5,40 +5,72 @@
 #include <SDL_mixer.h>
 #include <string>
 
+/**
+ * @class SoundEngine
+ * @brief A class responsible for playing sounds and music using SDL_mixer.
+ */
 class SoundEngine {
 private:
-    Mix_Music* backgroundMusic{nullptr};     // Pointer to background music track
-    Mix_Chunk* soundEffect{nullptr};         // Pointer to a sound effect
-    int musicVolume{2};                      // Represents the volume level (range [0, MIX_MAX_VOLUME])
-    int effectVolume{3};                     // Represents the volume level (range [0, MIX_MAX_VOLUME])
+    Mix_Music* backgroundMusic{nullptr};     ///< Pointer to background music track.
+    Mix_Chunk* soundEffect{nullptr};         ///< Pointer to a sound effect.
+    int musicVolume{2};                      ///< Volume level for music (range [0, MIX_MAX_VOLUME]).
+    int effectVolume{3};                     ///< Volume level for sound effects (range [0, MIX_MAX_VOLUME]).
 
 public:
+    /**
+     * @brief Default constructor.
+     * Initializes SDL_mixer with the desired audio settings.
+     */
     SoundEngine();
+
+    /**
+     * @brief Destructor.
+     * Cleans up and releases the resources.
+     */
     ~SoundEngine();
 
-    // Play a sound effect once
+    /**
+     * @brief Play a specific sound effect once.
+     * @param soundFilePath Path to the sound effect file.
+     */
     void playSoundEffect(const std::string& soundFilePath);
 
-    // Start background music in a loop
+    /**
+     * @brief Start playing background music in a loop.
+     * @param musicFilePath Path to the music file.
+     */
     void startBackgroundMusic(const std::string& musicFilePath);
 
-    // Stop the background music
+    /**
+     * @brief Stop the currently playing background music.
+     */
     void stopBackgroundMusic();
 
-    // Continue the background music
+    /**
+     * @brief Continue playing the background music if it's paused or stopped.
+     */
     void continueBackgroundMusic();
 
-    // Increase the volume
+    /**
+     * @brief Increase the volume of the background music.
+     */
     void increaseMusicVolume();
 
-    // Decrease the volume
+    /**
+     * @brief Decrease the volume of the background music.
+     */
     void decreaseMusicVolume();
 
-    // Increase the volume
+    /**
+     * @brief Increase the volume of sound effects.
+     */
     void increaseEffectVolume();
 
-    // Decrease the volume
+    /**
+     * @brief Decrease the volume of sound effects.
+     */
     void decreaseEffectVolume();
 };
+
 
 #endif //BOSCONIAN_SOUNDENGINE_H
