22cb6b3ddd20f4741370ac3f036b6790eaa8cc9a
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-10-05 10:17:26 +0200

    add comments for Fonts
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

--- src/Graphic/Fonts.h
+++ src/Graphic/Fonts.h
@@ -1,15 +1,23 @@
 #ifndef BOSCONIAN_FONTS_H
 #define BOSCONIAN_FONTS_H
 
+/**
+ * @namespace Font
+ * @brief Contains enumerations for different font types.
+ */
 namespace Font {
+
+    /**
+     * @enum Type
+     * @brief Represents different font sizes and styles for the "JOYSTIX" font.
+     */
     enum Type {
-        JOYSTIX_128PX,
-        JOYSTIX_64PX,
-        JOYSTIX_38PX,
-        JOYSTIX_24PX,
-        JOYSTIX_16PX,
-        // Space for more font options
-        MAX // Maximum number of font options
+        JOYSTIX_128PX,   ///< Joystix font at 128 pixels.
+        JOYSTIX_64PX,    ///< Joystix font at 64 pixels.
+        JOYSTIX_38PX,    ///< Joystix font at 38 pixels.
+        JOYSTIX_24PX,    ///< Joystix font at 24 pixels.
+        JOYSTIX_16PX,    ///< Joystix font at 16 pixels.
+        MAX              ///< Maximum number of font options.
     };
 }
 
