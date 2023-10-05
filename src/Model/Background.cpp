78108a6510e43395cf92a625234fe56cca302fc1
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-10-05 09:14:59 +0200

    add comments for Background
    Co-authored-by: deco42 <82336539+deco42@users.noreply.github.com>

--- src/Model/Background.cpp
+++ src/Model/Background.cpp
@@ -1,1 +1,50 @@
 #include "Background.h"
+
+Star::Star() {
+    randomizePosition();
+    randomizeSize();
+    randomizeColor();
+}
+
+void Star::randomizePosition() {
+    rect.x = Math::randomFloat(0, Config::levelWidth * Config::parallaxFactor); // 3000 * 0.6f
+    rect.y = Math::randomFloat(0, Config::levelHeight * Config::parallaxFactor); // 4000 * 0.6f
+}
+
+void Star::randomizeSize() {
+    rect.w = rect.h = Math::randomFloat(0.5f, 5.5f);
+}
+
+void Star::randomizeColor() {
+    color.r = Math::randomInt(0, 255);
+    color.g = Math::randomInt(0, 255);
+    color.b = Math::randomInt(0, 255);
+    color.a = Math::randomInt(0, 255);
+}
+
+Background::Background() {
+    stars.resize(numStars);
+}
+
+void Background::update(float deltaTime) {
+    elapsedTime += deltaTime * 1000; // Convert deltaTime to milliseconds
+    if (elapsedTime > randomizeDelay) {
+        int starsSize = stars.size();
+        int starBegin = starHalfToggle ? 0 : starsSize / 2;
+        int starEnd = starHalfToggle ? starsSize / 2 : starsSize;
+
+        for (size_t i = starBegin; i < starEnd; i++) {
+            auto& star = stars.at(i);
+
+            star.randomizePosition();
+            star.randomizeColor();
+        }
+
+        elapsedTime = 0;
+        starHalfToggle = !starHalfToggle;
+    }
+}
+
+std::vector<Star> Background::getStars() const {
+    return stars;
+}
--- src/Model/Background.h
+++ src/Model/Background.h
@@ -1,78 +1,76 @@
 #ifndef BOSCONIAN_BACKGROUND_H
 #define BOSCONIAN_BACKGROUND_H
 
-#include "../View/RenderEngine.h"
+#include <vector>
+#include <SDL.h>
+
 #include "Camera.h"
 #include "../Utilities/Math.h"
 #include "../Utilities/Config.h"
-#include "SDL.h"
-#include <vector>
 
+/**
+ * @class Star
+ * @brief Represents a star that will be displayed as part of the game's background.
+ *
+ * The star has properties related to its position, size, and color.
+ */
 class Star {
 public:
-    SDL_FRect rect;
-    SDL_Color color;
+    SDL_FRect rect;  //!< Star's position and size.
+    SDL_Color color; //!< Star's color.
 
-    Star() {
-        randomizePosition();
-        randomizeSize();
-        randomizeColor();
-    }
+    /**
+     * @brief Constructor that initializes a star with random properties.
+     */
+    Star();
 
-    void randomizePosition() {
-        rect.x = Math::randomFloat(0, Config::levelWidth * Config::parallaxFactor); // 3000 * 0.6f
-        rect.y = Math::randomFloat(0, Config::levelHeight * Config::parallaxFactor); // 4000 * 0.6f
-    }
+    /**
+     * @brief Randomizes the star's position.
+     */
+    void randomizePosition();
 
-    void randomizeSize() {
-        rect.w = rect.h = Math::randomFloat(0.5f, 5.5f);
-    }
+    /**
+     * @brief Randomizes the star's size.
+     */
+    void randomizeSize();
 
-    void randomizeColor() {
-        color.r = rand() % 256;
-        color.g = rand() % 256;
-        color.b = rand() % 256;
-        color.a = rand() % 256;
-    }
+    /**
+     * @brief Randomizes the star's color.
+     */
+    void randomizeColor();
 };
 
+/**
+ * @class Background
+ * @brief Represents the game's background, including all the stars.
+ *
+ * The background consists of a set number of stars, with the ability to update their properties periodically.
+ */
 class Background {
 private:
-    std::vector<Star> stars;
-    const int numStars = 256;
-    Uint64 elapsedTime = 0;
-    const Uint64 randomizeDelay = 750; // 0,75 seconds in milliseconds
-    bool starHalfToggle = false;
-
+    std::vector<Star> stars;               //!< Collection of stars forming the background.
+    const int numStars = 256;              //!< Number of stars in the background.
+    Uint64 elapsedTime = 0;                //!< Time elapsed since the last randomization.
+    const Uint64 randomizeDelay = 750;     //!< Delay in milliseconds to trigger star randomization.
+    bool starHalfToggle = false;           //!< Toggle to determine which half of the stars to randomize.
 
 public:
-    Background() {
-        stars.resize(numStars);
-    }
+    /**
+     * @brief Default constructor that initializes the background with a set number of stars.
+     */
+    Background();
 
-    void update(float deltaTime) {
-        elapsedTime += deltaTime * 1000; // Convert deltaTime to milliseconds
-        if (elapsedTime > randomizeDelay) {
-            int starsSize = stars.size();
-            int starBegin = starHalfToggle ? 0 : starsSize / 2;
-            int starEnd = starHalfToggle ? starsSize / 2 : starsSize;
+    /**
+     * @brief Updates the background state.
+     * @param deltaTime Time elapsed since the last frame (in seconds).
+     */
+    void update(float deltaTime);
 
-            for (size_t i = starBegin; i < starEnd; i++) {
-                auto& star = stars.at(i);
-
-                star.randomizePosition();
-                star.randomizeColor();
-            }
-
-            elapsedTime = 0;
-            starHalfToggle = !starHalfToggle;
-        }
-    }
-
-    std::vector<Star> getStars() const {
-        return stars;
-    }
-
+    /**
+     * @brief Fetches the collection of stars forming the background.
+     * @return Vector of stars.
+     */
+    std::vector<Star> getStars() const;
 };
 
 #endif //BOSCONIAN_BACKGROUND_H
